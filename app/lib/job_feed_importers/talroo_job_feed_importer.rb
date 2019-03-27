4cf45bd5ca088592e582212af9aaf711671cb389
Ron Cooke <ron@astonspark.com>
2019-03-26 23:26:35 -0400

    Stream XML file

--- app/lib/job_feed_importers/talroo_job_feed_importer.rb
+++ app/lib/job_feed_importers/talroo_job_feed_importer.rb
@@ -1,41 +1,58 @@
-require 'aws-sdk-s3'
+module JobFeedImporters
+  class TalrooJobFeedImporter
+    URL     = ENV['TALROO_URL']
+    OPTIONS = {
+      headers: {
+        'Content-Type' => 'text/xml',
+        'Accept'       => 'text/xml',
+      },
+      accept_encoding: 'gzip',
+    }.freeze
 
-class TalrooJobFeedImporter
-  URL = ENV['TALROO_URL']
-
-  def self.start
-    new.download
-  end
-
-  def self.import(s3_path)
-    new.import_file_from(s3_path)
-  end
-
-  def import_file(s3_path)
-    req = Typhoeus::Request.new(s3_path)
-    req.on_body do |chunk|
+    def self.start
+      new.import
     end
-    request.run
-  end
 
-  def download
-    feed_file = download_feed_file
-    feed_file_path = send_to_s3(feed_file)
-    enqueue_import(feed_file_path)
-  end
-
-  private
-
-  def download_feed_file
-    feed_file = nil
-    req = Typhoeus.get(URL)
-    req.on_complete do |res|
-      feed_file = res.body
+    def import
+      req = Typhoeus::Request.new(URL, OPTIONS)
+      req.on_body do |chunk|
+        process_chunk(chunk)
+      end
+      req.run
     end
-    feed_file
-  end
 
-  def send_to_s3(feed_file)
-  end
+    private
 
+    def process_chunk(chunk)
+      Nokogiri::XML::Reader(chunk).each do |node|
+        next unless node.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
+        next unless node.name == "job"
+        job_params = extract_job_params(node)
+        JobPosting.create(job_params)
+      end
+    end
+
+    def extract_job_params(node)
+      job_params = {}
+      fragment = Nokogiri::XML(node.outer_xml)
+
+      job_params[:status] = 'active'
+      job_params[:source] = 'talroo'
+      job_params[:source_identifier] = fragment.css('referencenumber').inner_text
+      job_params[:job_type] = parse_job_type(fragment.css('description').inner_text)
+      job_params[:company_name] = fragment.css('company').inner_text
+      job_params[:title] = fragment.css('title').inner_text
+      job_params[:description] = fragment.css('description').inner_text
+      job_params[:keywords] = parse_keywords(fragment)
+      job_params
+    end
+
+    def parse_job_type(description)
+      'full_time'
+    end
+
+    def parse_keywords(fragment)
+      ''
+    end
+  end
 end
