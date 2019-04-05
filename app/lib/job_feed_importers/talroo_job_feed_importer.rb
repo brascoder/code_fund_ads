0f8af3880e4d3984d19bdf5a8cc305d875e2097d
Ron Cooke <ron@astonspark.com>
2019-04-05 16:45:59 -0400

    Use description for keywords

--- app/lib/job_feed_importers/talroo_job_feed_importer.rb
+++ app/lib/job_feed_importers/talroo_job_feed_importer.rb
@@ -68,8 +68,9 @@ module JobFeedImporters
     end
 
     def extract_job_params(fragment)
-      keywords = parse_keywords(fragment)
-      # return nil if keywords.empty?
+      description = fragment.css('description').inner_text
+      keywords = parse_keywords(description)
+      return nil if keywords.empty?
 
       job_params = {}
       province_code, country_code = parse_province(fragment.css('state').inner_text)
@@ -80,7 +81,7 @@ module JobFeedImporters
       job_params[:job_type] = parse_job_type(fragment.css('title').inner_text)
       job_params[:company_name] = fragment.css('company').inner_text
       job_params[:title] = fragment.css('title').inner_text
-      job_params[:description] = fragment.css('description').inner_text
+      job_params[:description] = description
       job_params[:keywords] = keywords
       job_params[:min_annual_salary_cents] = 0
       job_params[:min_annual_salary_currency] = 'USD'
@@ -111,26 +112,11 @@ module JobFeedImporters
       ENUMS::JOB_TYPES::FULL_TIME
     end
 
-    def parse_keywords(fragment)
-      categories = []
+    def parse_keywords(description)
       keywords   = []
+      @tags.each { |tag| keywords << tag if description.include?(tag) }
 
-      %w(major minor).each do |type|
-        (0..2).each do |index|
-          category = "#{type}_category#{index}"
-          categories << fragment.css(category).inner_text
-        end
-      end
-
-      categories.map! { |cat| cat.delete('[').delete('[').downcase }
-      job_tags = @tags.filter { |tag| tag.length > 1 && categories.include?(tag) }
-      job_tags.each do |tag|
-        ENUMS::KEYWORDS.keys.each do |key|
-          keywords << key if ENUMS::KEYWORDS[key].include?(tag)
-        end
-      end
-
-      keywords.uniq.compact.sort
+      keywords
     end
 
     def parse_province(fragment)
